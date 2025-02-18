94130074ddfb5eb9f7afc87e1586c6fe01a125b8
Pavle Ralic <pavle.ralic@gmail.com>
2025-02-18 13:27:01 +0100

    Initial commit

---
+++ LiDARDepth/LiDARDepthApp.swift
@@ -0,0 +1,17 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The sample's main app.
+*/
+
+import SwiftUI
+
+@main
+struct LiDARDepthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
